67c92231d3681999af5d6a8590f60148553c30e2
nik0zer <s21b1.nikozer@gmail.com>
2022-09-19 23:05:27 +0300

    beautiful main.cpp

--- main.cpp
+++ main.cpp
@@ -5,31 +5,5 @@
 
 int main()
 {
-    stack a;
-    stack* b = &a;
-    stack_init(b, sizeof(int));
-    printf("%d\n", errno);
-
     test("test_data");
-
-    int c = 5, v = 6, j = 7, k = 8, p = 9;
-    printf("%d %d %d\n", b->num_of_alloc_stack_elem, b->num_of_elem, errno);
-    stack_push(b, &c);
-    memcpy(b->stack_pointer, &c, sizeof(c));
-    printf("%d %d %d\n", b->num_of_alloc_stack_elem, b->num_of_elem, errno);
-    stack_push(b, &c);
-    printf("%d %d %d\n", b->num_of_alloc_stack_elem, b->num_of_elem, errno);
-    stack_push(b, &c);
-    int* g = (int*)malloc(sizeof(int));
-    stack_pop(b, g);
-    printf("%d %d %d %d\n", *g, b->num_of_alloc_stack_elem, b->num_of_elem, errno);
-    memcpy(b->stack_pointer, &c, sizeof(c));
-    stack_pop(b, g);
-    printf("%d %d %d %d\n", *g, b->num_of_alloc_stack_elem, b->num_of_elem, errno);
-    memcpy(b->stack_pointer, &c, sizeof(c));
-    stack_pop(b, g);    
-    printf("%d %d %d %d\n", *g, b->num_of_alloc_stack_elem, b->num_of_elem, errno);
-    
-    free(g);
-    stack_destroy(b);
 }
